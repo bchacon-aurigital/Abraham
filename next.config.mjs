056916e98fd276174309f91ae40613a052662795
Marcela Cascante <marce@mainlydigitalworks.com>
2025-01-29 10:49:02 -0600

    Build Optimizations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone', // Creates a standalone build
+    poweredByHeader: false, // Removes the X-Powered-By header
+    compress: true, // Enable compression
+    images: {
+      formats: ['image/avif'], 
+      minimumCacheTTL: 60,
+    },
+    reactStrictMode: true,
+    swcMinify: true, // Use SWC for minification
+  }
+  
+  module.exports = nextConfig
