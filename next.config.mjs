774bbd51363b9c141f80c102f56387c68e36fb1f
Marcela Cascante <marce@mainlydigitalworks.com>
2025-01-29 11:03:50 -0600

    Before cleanup commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone', // Creates a standalone build
-    poweredByHeader: false, // Removes the X-Powered-By header
-    compress: true, // Enable compression
+    output: 'standalone',
+    poweredByHeader: false,
+    compress: true,
     images: {
-      formats: ['image/avif'], 
+      formats: ['image/avif'],
       minimumCacheTTL: 60,
     },
     reactStrictMode: true,
-    swcMinify: true, // Use SWC for minification
-  }
+    swcMinify: true,
+  };
   
-  module.exports = nextConfig
+  export default nextConfig;
